19ded122ca9a34f7e78146b2025a0cf857dc00c1
qtian13 <qiushuang.tian@gmail.com>
2021-09-01 00:51:43 -0700

    correct typo

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -20,7 +20,7 @@ const initialState = {
 }
 
 // TODO: To get a better understand of how a reducer works - add comments to the various actions in the reducer
-export const reducer = (state = initialState, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
     // TODO: Add a comment describing the functionality of the UPDATE_PRODUCTS case
     // Your comment here
@@ -104,3 +104,5 @@ export const reducer = (state = initialState, action) => {
 // export function useProductReducer(initialState) {
 //   return useReducer(reducer, initialState);
 // }
+
+export default reducer;
